a57c43da33887d7580a9417768d7476b8711f0f1
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 01:30:45 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	monopoly = {}
   monopoly[:railroads] = {
     :pieces => 4,
-    :names => {pennsylvania_railroad: {}, },
+    :names => {pennsylvania_railroad: {}, b_and_o_railroad: {}},
     :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
   }
   monopoly
